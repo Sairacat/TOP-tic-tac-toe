17db862fe938d4283d5671b7499391677f6a8cb0
Sairacat <201769jzy@gmail.com>
2026-05-22 16:31:11 +0800

    I reworked the codes about manipulating DOM elemen t. Put all of these codes inside a displayController IIFE. The game IIFE will only focus on the logic behind this game, everything apart from that will be excluded from it. I don't know wether it's necessary to do that, but this separation kinda neat, it feels clean.

--- script.js
+++ script.js
@@ -1,5 +1,83 @@
 'use strict'
 
+const displayController = (function() {
+    const domElemnt = {
+        board: document.querySelector('.gameboard'),
+        restartBtn: document.querySelector('.restart-btn'),
+        grids: document.querySelectorAll('.gamegrid'),
+        pointsOfX: document.querySelector('.pointsofx'),
+        pointsOfO: document.querySelector('.pointsofo'),
+        messageOfX: document.querySelector('.messageofx'),
+        messageOfO: document.querySelector('.messageofo'),
+        xSide: document.querySelector('.x-side'),
+        oSide: document.querySelector('.o-side')
+    }
+
+    const eventHandler = function(clickGrid, clickRestart) {
+        domElemnt.board.addEventListener('click', function(e) {
+            if(!e.target.classList.contains('gamegrid')) return;
+            let index = Number(e.target.id);
+            const marker = clickGrid(index);
+            if(marker === 'X') {
+                e.target.textContent = '❌';
+            }else if(marker === 'O') {
+                e.target.textContent = '⭕️';
+            }else {
+                return;
+            }
+
+            showWinnerMessage();
+            updatePoints();
+        })
+
+        domElemnt.restartBtn.addEventListener('click',function(e) {
+            clickRestart();
+            clearDisplay();
+        })
+    }
+
+    const showWinnerMessage = function() {
+        const winner = game.showWinner();
+        const gameStatus = game.showGameStatus();
+        if(gameStatus) {
+            if(winner === 'X') {
+                domElemnt.messageOfX.textContent = 'X has Won!!';
+                domElemnt.messageOfO.textContent = 'O has Lost!!';
+                domElemnt.xSide.classList.add('won');
+                domElemnt.oSide.classList.add('lost');
+            }else if(winner === 'O') {
+                domElemnt.messageOfX.textContent = 'X has Lost!!';
+                domElemnt.messageOfO.textContent = 'O has Won!!';
+                domElemnt.xSide.classList.add('lost');
+                domElemnt.oSide.classList.add('won')
+            }else {
+                domElemnt.messageOfX.textContent = 'It\'s a draw';
+                domElemnt.messageOfO.textContent = 'It\'s a draw';
+            }
+        }else {
+            return;
+        }
+    }
+
+    const updatePoints = function() {
+        domElemnt.pointsOfX.textContent = game.showPointsOfX();
+        domElemnt.pointsOfO.textContent = game.showPointsOfO();
+    }
+
+    const clearDisplay = function() {
+        for(const gamegrid of domElemnt.grids) {
+            gamegrid.textContent = '';
+        }
+
+        domElemnt.messageOfX.textContent = '';
+        domElemnt.messageOfO.textContent = '';
+        domElemnt.xSide.classList.remove('won', 'lost');
+        domElemnt.oSide.classList.remove('won', 'lost');
+    }
+
+    return {eventHandler}
+})()
+
 function makeGrid() {
     const clicked = false;
     return {clicked};
@@ -15,8 +93,7 @@ const game = (function() {
     let pointOfO = 0;
     let step = 0;
     let gameFinshed = false;
-    let xHasWon = false;
-    let oHasWon = false;
+    let winner;
     
     const selectedByX = [];
     const selectedByO = [];
@@ -32,8 +109,11 @@ const game = (function() {
         gameboard.push(grid);
     }
 
-    const play = function(e) {
-        let index = Number(e.target.id);
+    const play = function(index) {
+        if(gameFinshed) {
+            return;
+        }
+        let marker;
         console.log(`index is ${index}`);
         if(gameboard[index].clicked) {
             console.log('This grid has been clicked! Choose another one!');
@@ -44,11 +124,13 @@ const game = (function() {
                 selectedByX.push(index);
                 step++;
                 console.log('X has moved');
+                marker = 'X';
             }else if(isO) {
                 gameboard[index].clicked = true;
                 selectedByO.push(index);
                 step++;
                 console.log('O has moved');
+                marker = 'O';
             }
 
         }
@@ -60,34 +142,20 @@ const game = (function() {
         console.log(step);
         console.log(selectedByX);
         console.log(selectedByO);
-    }
-
-    const showTurn = function() {
-        return isX;
+        return marker;
     }
 
     const restart = function() {
         for(const grid of gameboard) {
             grid.clicked = false;
         }
-        for(const gamegrid of gameGrids) {
-            gamegrid.textContent = '';
-        }
-
         selectedByX.length = 0;
         selectedByO.length = 0;
         step = 0;
         isX = true;
         isO = false;
         gameFinshed = false;
-        xHasWon = false;
-        oHasWon = false;
-        messageOfX.textContent = '';
-        messageOfO.textContent = '';
-        xSide.classList.remove('won', 'lost');
-        oSide.classList.remove('won', 'lost');
-
-
+        winner = '';
         console.log('Game has restarted!')
     }
 
@@ -104,7 +172,8 @@ const game = (function() {
                         pointOfX++;
                         console.log('X win!');
                         gameFinshed = true;
-                        xHasWon = true;
+                        winner = 'X'
+                        break;
                     }
                 }
             }else if(isO) {
@@ -113,7 +182,8 @@ const game = (function() {
                         pointOfO++;
                         console.log('O win!');
                         gameFinshed = true;
-                        oHasWon = true;
+                        winner = 'O'
+                        break;
                     }
                 }
             }
@@ -125,59 +195,50 @@ const game = (function() {
                         pointOfX++;
                         console.log('X win!');
                         gameFinshed = true;
-                        xHasWon = true;
+                        winner = 'X'
+                        break;
                     }
                 }
 
-                if(!xHasWon){
+                if(!gameFinshed){
                 console.log('It\'s a draw')
                 gameFinshed = true;
                 }
         }
-
-        updatePoints();
-        showWinnerMessage();
     }
 
-    const showGameHasFinishedOrNot = function() {
+    const showWinner = function() {
+        return winner;
+    }
+
+    const showPointsOfX = function() {
+        return pointOfX;
+    }
+
+    const showPointsOfO = function() {
+        return pointOfO;
+    }
+
+    const showGameStatus = function() {
         return gameFinshed;
-    }
-
-    const showWinnerMessage = function() {
-        if(gameFinshed) {
-            if(xHasWon) {
-                messageOfX.textContent = 'X has Won!!';
-                messageOfO.textContent = 'O has Lost!!';
-                xSide.classList.add('won');
-                oSide.classList.add('lost');
-            }else if(oHasWon) {
-                messageOfX.textContent = 'X has Lost!!';
-                messageOfO.textContent = 'O has Won!!';
-                xSide.classList.add('lost');
-                oSide.classList.add('won')
-            }else {
-                messageOfX.textContent = 'It\'s a draw';
-                messageOfO.textContent = 'It\'s a draw';
-            }
-        }else {
-            return;
-        }
-    }
-
-    const updatePoints = function() {
-        domPointsOfX.textContent = pointOfX;
-        domPointsOfO.textContent = pointOfO;
     }
 
     return {
         play,
-        showTurn,
         restart,
-        showGameHasFinishedOrNot
+        showWinner,
+        showPointsOfX,
+        showPointsOfO,
+        showGameStatus
     }
 })()
 
 
+displayController.eventHandler(game.play, game.restart);
+
+
+
+/* 
 const domGridBoard = document.querySelector('.gameboard');
 const restartBtn = document.querySelector('.restart-btn');
 const gameGrids = document.querySelectorAll('.gamegrid');
@@ -212,3 +273,4 @@ domGridBoard.addEventListener('click', function(e) {
 restartBtn.addEventListener('click', () => {
     game.restart();
 })
+*/
